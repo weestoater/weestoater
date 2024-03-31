855cb60c90ad88511610b158503438ddcdf50d7a
Ian Burrett <ian@weestoater.com>
2024-03-31 13:31:37 +0100

    refreshed a11y content

--- src/content/a11y/a11yme.js
+++ src/content/a11y/a11yme.js
@@ -15,19 +15,21 @@ export const A11yMe = () => {
 
         <p>
           I <strong>really care</strong> about Accessibility (a11y) online and
-          have done for over 20 years now.
+          have done for 20+ years now.
         </p>
 
         <p>
-          With a move to a UX team in Sep '21, I've switched direction as a
-          developer, instead of being a delivery focused engineer, I'm now
+          With a move to a UX team in Sep '21, I've switched direction as a web
+          developer, instead of being a delivery focused front-end engineer, I'm
           working with teams as a <strong>SME</strong> (subject matter expert)
-          on UI development and more importantly Web Standards &amp; a11y.
+          on UI development and more importantly Web Standards &amp; a11y. As
+          well as mentoring junior talent in best practices and sharing my
+          extensive experience.
         </p>
 
         <p>
-          I'm now working alongside a team of designers servicing their needs
-          for developer input / advice but also educating and supporting the
+          I'm working alongside a team of designers servicing their needs for
+          developer input / advice but also educating and supporting the
           products the designers work to.
         </p>
       </div>
