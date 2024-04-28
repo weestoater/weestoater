f703241fc77a071b030e4c4ae34ca7717a221570
Ian Burrett <ian@weestoater.com>
2024-04-28 13:36:21 +0100

    updated football & new react content

--- src/pages/React.js
+++ src/pages/React.js
@@ -5,12 +5,16 @@ import { PageTitleH1 } from "../components/global/pageTitleHeading";
 import { ReactHooksPost } from "../content/react/ReactHooks";
 import { NextThingPost } from "../content/react/NextThingPost";
 import { SaltPost } from "../content/react/SaltPost";
+import { ViteContent } from "../content/react/ViteReact";
 
 export const ReactPage = () => {
   return (
     <>
       <PageTitleH1 title="React" />
       <GridLayout columns={{ xs: 1, sm: 2, md: 3, lg: 3, xl: 4 }}>
+        <GridItem>
+          <ViteContent />
+        </GridItem>
         <GridItem>
           <SaltPost />
         </GridItem>
